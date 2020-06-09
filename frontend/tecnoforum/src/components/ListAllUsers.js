39dcf54fc45875163df4cb3f55b4d3a488019713
YosKa <joni.karilainen@gmail.com>
2020-06-09 19:13:49 +0300

    add Spinner

--- frontend/tecnoforum/src/components/ListAllUsers.js
+++ frontend/tecnoforum/src/components/ListAllUsers.js
@@ -1,41 +1,39 @@
 import React from 'react';
-import Row from './Row'
-import {Table} from 'semantic-ui-react';
-import DataFetchController from '../controller/DataFetchController'
+import Row from './Row';
+import { Table } from 'semantic-ui-react';
+import DataFetchController from '../controller/DataFetchController';
+import Spinner from './Spinner';
 
 export default class ListAllUsers extends React.Component {
-	
-	constructor(props) {
-		super(props);
-		this.state = {
-			list:[]
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+  }
 
-	componentDidMount() {
-		DataFetchController.getUserslist(this);
-	}
+  componentDidMount() {
+    DataFetchController.getUserslist(this);
+  }
 
-	render() {
-		let users = this.state.list.map((user,index) => {
-			return <Row key={user.id} item={user}/>
-		})
-		return(
-			<Table celled>
-				<Table.Header>
-				<Table.Row>
-					<Table.HeaderCell>Fullname</Table.HeaderCell>
-					<Table.HeaderCell>Password</Table.HeaderCell>
-					<Table.HeaderCell>Email</Table.HeaderCell>
-					<Table.HeaderCell>Nickname</Table.HeaderCell>
-					<Table.HeaderCell>Id</Table.HeaderCell>
-				</Table.Row>
-				</Table.Header>
-				<Table.Body>
-				{users}
-				</Table.Body>
-			</Table>
-		)
-	}
-
+  render() {
+    if (this.state.list <= this.state.list.length) return <Spinner />;
+    let users = this.state.list.map((user, index) => {
+      return <Row key={user.id} item={user} />;
+    });
+    return (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Fullname</Table.HeaderCell>
+            <Table.HeaderCell>Password</Table.HeaderCell>
+            <Table.HeaderCell>Email</Table.HeaderCell>
+            <Table.HeaderCell>Nickname</Table.HeaderCell>
+            <Table.HeaderCell>Id</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{users}</Table.Body>
+      </Table>
+    );
+  }
 }
