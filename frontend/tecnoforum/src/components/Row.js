e4caa85e3f28d49f359ac209c14dd58e7e712fa4
YosKa <joni.karilainen@gmail.com>
2020-06-09 16:17:11 +0300

    Destructuring to cleaner code

--- frontend/tecnoforum/src/components/Row.js
+++ frontend/tecnoforum/src/components/Row.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Table} from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
 
 export default class Row extends React.Component {
-	
-	render() {
-		return (
-			<Table.Row>
-				<Table.Cell>{this.props.item.fullname}</Table.Cell>
-				<Table.Cell>{this.props.item.password}</Table.Cell>
-				<Table.Cell>{this.props.item.email}</Table.Cell>
-				<Table.Cell>{this.props.item.nickname}</Table.Cell>
-				<Table.Cell>{this.props.item.id}</Table.Cell>
-			</Table.Row>
-		)
-	}				
+  render() {
+    const { fullname, password, email, nickname, id } = this.props.item;
+    return (
+      <Table.Row>
+        <Table.Cell>{fullname}</Table.Cell>
+        <Table.Cell>{password}</Table.Cell>
+        <Table.Cell>{email}</Table.Cell>
+        <Table.Cell>{nickname}</Table.Cell>
+        <Table.Cell>{id}</Table.Cell>
+      </Table.Row>
+    );
+  }
 }
