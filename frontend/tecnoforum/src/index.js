84232174759052d6da8606ae83b0e04da126ed7e
YosKa <joni.karilainen@gmail.com>
2020-06-11 08:06:52 +0300

    edit src/index to functional. start Redux

--- frontend/tecnoforum/src/index.js
+++ frontend/tecnoforum/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import rootReducer from './reducers/';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <App />
-	</BrowserRouter>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
