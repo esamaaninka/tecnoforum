a791e6e35030381dd8280f5e58ab0320af474a1c
YosKa <joni.karilainen@gmail.com>
2020-06-11 08:10:04 +0300

    Frontend: edit App to functional component

--- frontend/tecnoforum/src/App.js
+++ frontend/tecnoforum/src/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Switch, Route, Redirect} from 'react-router-dom';
-import {withRouter} from 'react-router-dom';
-import ListAllUsers from './components/ListAllUsers'
+import { Switch, Route } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
+import ListAllUsers from './components/ListAllUsers';
 import GetUser from './components/GetUser';
 import Registration from './components/Registration';
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-
-		};
-	}
-
-	render() {
-		return (
-			<div>
-			<h1>_TecnoForum_</h1>{' '}
-			<Switch>
-				<Route exact path="/" render={() => (<ListAllUsers/>)}/>
-				<Route exact path="/user/:name" render={(props) => (<GetUser name={props.match.params.name}/>)}/>
-				<Route exact path="/register/" render={() => (<Registration history={this.props.history}/>)}/>
-				<Route render={() => (<ListAllUsers/>)}/>
-			</Switch>
-			</div>
-		);
-	}
-}
+const App = ({ history }) => {
+  return (
+    <div>
+      <h1>_TecnoForum_</h1>{' '}
+      <Switch>
+        <Route exact path='/' render={() => <ListAllUsers />} />
+        <Route exact path='/user/:name' render={(props) => <GetUser name={props.match.params.name} />} />
+        <Route exact path='/register/' render={() => <Registration history={history} />} />
+        <Route render={() => <ListAllUsers />} />
+      </Switch>
+    </div>
+  );
+};
 
 export default withRouter(App);
