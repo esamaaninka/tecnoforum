0c817a17483943058d4f7c5aef0b733895f31977
esamaaninka <61664607+esamaaninka@users.noreply.github.com>
2020-06-27 12:49:02 +0300

    added option to define local monbodb url in  .env, if not set Atlas MongoDB used.

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT || 3001
-let MONGODB_URI = process.env.MONGODB_URI 
+let MODE = process.env.mode 
+
+let MONGODB_URI =  !MODE ? process.env.MONGODB_URI : process.env.MONGODB_LOCAL_URI 
 
 module.exports = {
   MONGODB_URI,
